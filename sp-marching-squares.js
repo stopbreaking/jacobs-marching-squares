aaaab70047bc69e17cbe2e720ef5a720ae3b3ca6
LimeJacob <stopbreakingplease@gmail.com>
2025-12-17 17:00:56 -0500

    Update sp-marching-squares.js

--- sp-marching-squares.js
+++ sp-marching-squares.js
@@ -9,39 +9,3 @@ document.querySelector('#app3').innerHTML = `
     </p>
   </div>
 `
-setTimeout(() => {
-    CDP(async (client) => {
-        const {Network, Page, Runtime} = client;
-
-        const requests = new Set(); // <---------- HERE
-
-        Network.requestWillBeSent(({requestId, request}) => {
-            if(request.url.indexOf("ct2/results/rpc") != -1){
-                console.log(`REQ [${requestId}] ${request.method} ${request.url} \n`);
-
-                requests.add(requestId); // <---------- HERE
-
-            }
-        });
-        Network.loadingFinished(async ({requestId}) => {
-
-            if (requests.has(requestId)) { // <---------- HERE
-
-                const {body, base64Encoded} = await Network.getResponseBody({requestId});
-                console.log(`RES [${requestId}] body: ${body} \n`);
-            }
-        });
-        try {
-            await Promise.all([Network.enable(), Page.enable()]);
-            await Page.navigate({url: 'https://clinicaltrials.gov/ct2/results?cond=Parents&term=&cntry1=&state1=&Search=Search&recrs=a#wrapper'});
-            await Page.loadEventFired();
-            await Runtime.evaluate({
-                expression: `document.querySelector('.paginate_button.next').click()`
-            });
-        } catch (err) {
-            console.error(err);
-        }
-    }).on('error', (err) => {
-        console.error(err);
-    });
-}, 1000);
